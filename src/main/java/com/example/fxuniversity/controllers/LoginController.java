66a2aa9767c95310f4fca3a0b89d83734477e338
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-10-02 11:20:58 +0100

    Added functionality to login screen

--- src/main/java/com/example/fxuniversity/controllers/LoginController.java
+++ src/main/java/com/example/fxuniversity/controllers/LoginController.java
@@ -1,12 +1,22 @@
 package com.example.fxuniversity.controllers;
 
+import com.example.fxuniversity.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    private AnchorPane anchorPane;
+
     @FXML
     private Button btnAdmin;
 
@@ -20,17 +30,26 @@ public class LoginController {
     private HBox hbxButtons;
 
     @FXML
-    void onAdminLogin(ActionEvent event) {
-
+    void onAdminLogin(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("admin-view.fxml"));new Scene(fxmlLoader.load());
+        showScene(fxmlLoader);
     }
 
     @FXML
-    void onProfessorLogin(ActionEvent event) {
-
+    void onProfessorLogin(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("professor-view.fxml"));
+        showScene(fxmlLoader);
     }
 
     @FXML
-    void onStudentLogin(ActionEvent event) {
+    void onStudentLogin(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("student-view.fxml"));
+        showScene(fxmlLoader);
+    }
 
+    private void showScene(FXMLLoader loader) throws IOException {
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.setScene(scene);
     }
 }
