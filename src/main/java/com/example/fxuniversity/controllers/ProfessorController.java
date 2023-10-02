755e2576157b18003d4937591927ccc59705e34f
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-10-02 11:22:17 +0100

    Added basic professor UI

--- src/main/java/com/example/fxuniversity/controllers/ProfessorController.java
+++ src/main/java/com/example/fxuniversity/controllers/ProfessorController.java
@@ -1,4 +1,11 @@
 package com.example.fxuniversity.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 public class ProfessorController {
-}
+    @FXML
+    private Button addGrade;
+
+    @FXML
+    private Button getSchedule;
+    }
