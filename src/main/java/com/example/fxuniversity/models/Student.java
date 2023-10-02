4957ee4446e6994625a935b80fa9ba0780ff39cd
Ayush <pandeyayushraj4@gmail.com>
2023-10-02 10:22:40 +0100

    Created Course and Student Class

--- src/main/java/com/example/fxuniversity/models/Student.java
+++ src/main/java/com/example/fxuniversity/models/Student.java
@@ -1,4 +1,46 @@
 package com.example.fxuniversity.models;
 
+import java.util.Scanner;
+
 public class Student {
+
+    private String studentName, studentAddress, studentMajor, studentEmail, studentPhoneNumber;
+
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public void setStudentPhoneNumber(String studentPhoneNumber) {
+        this.studentPhoneNumber = studentPhoneNumber;
+    }
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public String getStudentMajor() {
+        return studentMajor;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public String getStudentPhoneNumber() {
+        return studentPhoneNumber;
+    }
+
+    public Student(String studentName, String studentAddress, String studentMajor, String studentEmail, String studentPhoneNumber) {
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+        this.studentMajor = studentMajor;
+        this.studentEmail = studentEmail;
+        this.studentPhoneNumber = studentPhoneNumber;
+    }
 }
