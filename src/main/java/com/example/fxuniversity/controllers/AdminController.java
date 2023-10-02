6e21323a0a3cc60f868b7a2f5c01cf8ed34a6b4c
Ayush <pandeyayushraj4@gmail.com>
2023-10-02 11:16:43 +0100

    Created an admin view with some buttons.

--- src/main/java/com/example/fxuniversity/controllers/AdminController.java
+++ src/main/java/com/example/fxuniversity/controllers/AdminController.java
@@ -1,4 +1,23 @@
 package com.example.fxuniversity.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class AdminController {
+
+    @FXML
+    Button addStudent = new Button();
+
+    @FXML
+    Button deleteStudent = new Button();
+
+    @FXML
+    Text dummyText = new Text();
+
+    @FXML
+    public void onAddStudent (ActionEvent event) {
+        dummyText.setText("Student was added");
+    }
 }
