98d4c38357a1db91603b382929e9ce4b137d8682
abhi1 <abhi1904kale@gmail.com>
2023-10-02 10:25:31 +0100

    Added Department and professor constructors

--- src/main/java/com/example/fxuniversity/models/Department.java
+++ src/main/java/com/example/fxuniversity/models/Department.java
@@ -1,4 +1,28 @@
 package com.example.fxuniversity.models;
 
 public class Department {
+
+    private String name;
+
+    private String professorindep;
+
+    private String courseindep;
+
+    public Department(String name, String professorindep, String courseindep) {
+        this.name = name;
+        this.professorindep = professorindep;
+        this.courseindep = courseindep;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfessorindep() {
+        return professorindep;
+    }
+
+    public String getCourseindep() {
+        return courseindep;
+    }
 }
--- src/main/java/com/example/fxuniversity/models/Professor.java
+++ src/main/java/com/example/fxuniversity/models/Professor.java
@@ -1,4 +1,45 @@
 package com.example.fxuniversity.models;
 
 public class Professor {
+
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String department;
+    private int officenumber;
+
+
+    public Professor(String name, String address, String phone, String email, String department, int officenumber) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.department = department;
+        this.officenumber = officenumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getOfficenumber() {
+        return officenumber;
+    }
 }
