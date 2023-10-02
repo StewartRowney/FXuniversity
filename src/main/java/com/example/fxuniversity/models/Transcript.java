bc8ffdcb6ffa67ffbe73590e7a46a9e0ebce8841
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-10-02 13:50:03 +0100

    Added UUID class/student into Transcript class

--- src/main/java/com/example/fxuniversity/models/Transcript.java
+++ src/main/java/com/example/fxuniversity/models/Transcript.java
@@ -1,20 +1,33 @@
 package com.example.fxuniversity.models;
 
+import java.util.UUID;
+
 public class Transcript {
 
-    private Course course;
+    private UUID classID;
+    private UUID studentID;
     private String grade;
 
-    public Transcript(Course course, String grade) {
-        this.course = course;
+
+    public Transcript(UUID classID, UUID studentID, String grade) {
+        this.classID = classID;
+        this.studentID = studentID;
         this.grade = grade;
     }
 
-    public Course getCourse() {
-        return course;
+
+    public UUID getClassID() {
+        return classID;
     }
-    public void setCourse(Course course) {
-        this.course = course;
+    public void setClassID(UUID classID) {
+        this.classID = classID;
+    }
+
+    public UUID getStudentID() {
+        return studentID;
+    }
+    public void setStudentID(UUID studentID) {
+        this.studentID = studentID;
     }
 
     public String getGrade() {
