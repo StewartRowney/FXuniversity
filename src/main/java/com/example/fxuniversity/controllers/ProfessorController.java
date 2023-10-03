2867af32c81589ad89750706bdc0ac15d1a02112
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-10-03 11:34:35 +0100

    Updated professor UI/controller with basic functionality.

--- src/main/java/com/example/fxuniversity/controllers/ProfessorController.java
+++ src/main/java/com/example/fxuniversity/controllers/ProfessorController.java
@@ -1,17 +1,121 @@
 package com.example.fxuniversity.controllers;
 
 import com.example.fxuniversity.models.Professor;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+
 public class ProfessorController {
 
     private Professor currentProfessor;
 
     @FXML
-    private Button addGrade;
+    private Button btnConfirmAddGrade;
 
     @FXML
-    private Button getSchedule;
+    private Button btnProfessorLogout;
+
+    @FXML
+    private Button btnSchedule;
+
+    @FXML
+    private Label lblClassListAddGrade;
+
+    @FXML
+    private Label lblSelectedClass;
+
+    @FXML
+    private Label lblSelectedDate;
+
+    @FXML
+    private Label lblStudentListAddGrade;
+
+    @FXML
+    private Label lblWelcomeProfessor;
+
+    @FXML
+    private ListView<?> listViewClassesAddGrade;
+
+    @FXML
+    private ListView<?> listViewClassesSchedule;
+
+    @FXML
+    private ListView<?> listViewDateSchedule;
+
+    @FXML
+    private ListView<?> listViewStudents;
+
+    @FXML
+    private RadioButton rdioBtnGradeA;
+
+    @FXML
+    private RadioButton rdioBtnGradeB;
+
+    @FXML
+    private RadioButton rdioBtnGradeC;
+
+    @FXML
+    private RadioButton rdioBtnGradeD;
+
+    @FXML
+    private RadioButton rdioBtnGradeE;
+
+    @FXML
+    private RadioButton rdioBtnGradeF;
+
+    @FXML
+    private Tab tbProfessorAddGrade;
+
+    @FXML
+    private Tab tbProfessorHomepage;
+
+    @FXML
+    private Tab tbProfessorSchedule;
+
+    @FXML
+    void onConfirmAddGrade(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSeeSchedule(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeA(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeC(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeD(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeE(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetGradeF(ActionEvent event) {
+
+    }
 
     public void setProfessor(Professor professor) {
         this.currentProfessor = professor;
