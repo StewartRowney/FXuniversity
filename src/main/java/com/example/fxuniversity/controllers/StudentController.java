f9c771458ed13e93d5d9dac5a1448076f2d4493b
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-10-03 09:38:23 +0100

    Updated student controller/UI with basic functionality

--- src/main/java/com/example/fxuniversity/controllers/StudentController.java
+++ src/main/java/com/example/fxuniversity/controllers/StudentController.java
@@ -2,15 +2,54 @@ package com.example.fxuniversity.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 public class StudentController {
     @FXML
-    private Button btnStudent;
+    private Button btnCourseDescription;
 
-    public void onStudentLogin(ActionEvent actionEvent) {
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    private Button btnSeeCourseList;
+
+    @FXML
+    private ListView<?> listViewCourses;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private AnchorPane tbCourseList;
+
+    @FXML
+    private Tab tbStudentHomepage;
+
+    @FXML
+    private TextArea txtAreaCourseDescription;
+
+    @FXML
+    void onCourseDescription(ActionEvent event) {
+
     }
 
-    public void onStudentLogout(ActionEvent actionEvent) {
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRegisterCourse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSeeCourseList(ActionEvent event) {
+
     }
 }
