525b92b55354a2f61d1e538893b72e93e7dc2f2f
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-10-03 14:21:27 +0100

    Refactored Admin UI and added button logic

--- src/main/java/com/example/fxuniversity/controllers/AdminController.java
+++ src/main/java/com/example/fxuniversity/controllers/AdminController.java
@@ -1,21 +1,50 @@
 package com.example.fxuniversity.controllers;
 
+import com.example.fxuniversity.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
 
     @FXML
-    private Button btnAdmit;
+    private AnchorPane anchorPane;
 
     @FXML
-    private Button btnDelete;
+    private Button btnAddClass;
+
+    @FXML
+    private Button btnAddCourse;
+
+    @FXML
+    private Button btnAdmitStudent;
+
+    @FXML
+    private Button btnDeleteClass;
+
+    @FXML
+    private Button btnDeleteCourse;
+
+    @FXML
+    private Button btnHome;
 
     @FXML
     private Button btnLogout;
+
+    @FXML
+    private Button btnManageCourse;
+
+    @FXML
+    private Button btnScheduleClass;
+
+    @FXML
+    private Button btnStudentAdmit;
 
     @FXML
     private TextField getStudentAddress;
@@ -33,20 +62,84 @@ public class AdminController {
     private TextField getStudentPhone;
 
     @FXML
-    private TextField setPrerequisite;
+    private TabPane tabPane;
 
     @FXML
-    void onAdminLogout(ActionEvent event) {
+    private Tab tbAddClass;
 
+    @FXML
+    private Tab tbAddCourse;
+
+    @FXML
+    private Tab tbDeleteClass;
+
+    @FXML
+    private Tab tbDeleteCourse;
+
+    @FXML
+    private Tab tbHome;
+
+    @FXML
+    private Tab tbManageCourse;
+
+    @FXML
+    private Tab tbScheduleClass;
+
+    @FXML
+    private Tab tbStudentAdmit;
+
+    @FXML
+    void OnAddClass(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbAddClass);
     }
 
     @FXML
-    void onAdmit(ActionEvent event) {
-
+    void OnAddCourse(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbAddCourse);
     }
 
     @FXML
-    void onDelete(ActionEvent event) {
-
+    void OnAdmitStudent(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbStudentAdmit);
     }
+
+    @FXML
+    void OnDeleteClass(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbDeleteClass);
+    }
+
+    @FXML
+    void OnScheduleClass(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbScheduleClass);
+    }
+
+    @FXML
+    void onDeleteCourse(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbDeleteCourse);
+    }
+
+    @FXML
+    void onHomeAction(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbHome);
+    }
+
+    @FXML
+    void onManageCourse(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbManageCourse);
+    }
+
+    @FXML
+    void onStudentAdmit(ActionEvent event) {
+        tabPane.getSelectionModel().select(tbStudentAdmit);
+    }
+
+    @FXML
+    void onAdminLogout(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
+
 }
